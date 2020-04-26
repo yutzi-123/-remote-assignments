d45512f8d29b827dc1f28b778ef9325066915cab
yutzi-123 <55330961+yutzi-123@users.noreply.github.com>
2020-04-26 15:17:43 +0800

    Update javascript.js

--- week02/javascript.js
+++ week02/javascript.js
@@ -1,8 +1,8 @@
 /*When user click on "Welcome Message" block, change text to "Have a Good Time!".*/
 const welcomeMsg = document.querySelector('header')
-welcomeMsg.addEventListener('click', function () {
+welcomeMsg.addEventListener('click',function (){
   welcomeMsg.innerHTML = `Have a Good Time!`
-
+  
 })
 
 /*When user clicks the menu at the top-right corner, show the hidden mobile menu. After that, the user can click the close button to hide it.*/
@@ -10,11 +10,11 @@ const menu = document.querySelector('.menu')
 const hiddenMenu = document.querySelector('.hiddenMenu')
 const cancle = document.querySelector('.X')
 
-menu.addEventListener('click', function () {
+menu.addEventListener('click',function(){
   hiddenMenu.style.display = 'inline-block';
 })
 
-cancle.addEventListener('click', function () {
+cancle.addEventListener('click',function(){
   hiddenMenu.style.display = 'none';
 })
 
@@ -23,7 +23,9 @@ cancle.addEventListener('click', function () {
 const btn = document.querySelector('.btn')
 const hiddenBox = document.querySelector('.hiddenBox')
 
-btn.addEventListener('click', function () {
+btn.addEventListener('click',function(){
   hiddenBox.style.display = 'block';
 })
+
+
 
